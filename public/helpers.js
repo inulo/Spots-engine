dbbb64b4ff9beecf995333090422f74479b200c2
V. S. Kanchev <27562113+inulo@users.noreply.github.com>
2022-03-12 20:19:59 +0000

    up

--- public/helpers.js
+++ public/helpers.js
@@ -10,11 +10,9 @@ const createVenueHTML = (name, location, iconSource) => {
   const createWeatherHTML = (currentDay) => {
     console.log(currentDay)
     return `<h2>${weekDays[(new Date()).getDay()]}</h2>
-          <h2>Temperature: ${Kelvin(currentDay.main.temp)}&deg;F</h2>
+          <h2>Temperature: ${kelvinToFahrenheit(currentDay.main.temp)}&deg;F</h2>
           <h2>Condition: ${currentDay.weather[0].description}</h2>
         <img src="https://openweathermap.org/img/wn/${currentDay.weather[0].icon}@2x.png">`;
   }
   
-  // const kelvinToFahrenheit = k => ((k - 273.15) * 9 / 5 + 32).toFixed(0);
-  Kelvin = Celcius + 273.15
-  Kelvin = ((Fahrenheit - 32) / 1.8) + 273.15
+  const kelvinToFahrenheit = k => ((k - 273.15) * 9 / 5 + 32).toFixed(0);
